452d916b275d3b4be160662adccf2647a3e07be0
Maram Ismail <maramismail2015@gmail.com>
2022-04-17 21:04:50 +0300

    ex 2

--- useeffect/src/components/Mousemove.jsx
+++ useeffect/src/components/Mousemove.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import React , {useEffect ,useState} from 'react'
 
 export default function Mousemove() {
+  const [width , setWidth] = useState({
+    widthX:0,
+    widthY:0,
+  });
+   const [backgroundColor , setBackgroundColor] = useState('')
+    
+   
+   const backgroundColorHandler = (e) =>{
+     let mouseX = e.screenX ;;
+     let mouseY = e.screenY;
+    if(mouseX > window.innerWidth/2){
+      setBackgroundColor('blue')
+    }else{
+      setBackgroundColor('tomato')
+    }
+     setWidth({mouseX,mouseY})
+   };
+
+   useEffect (() => {
+    document.addEventListener('mousemove' , backgroundColorHandler);
+    return() =>{
+      document.removeEventListener('mousemove' , backgroundColorHandler)
+    }
+   });
+
   return (
-    <div>Mousemove</div>
+    <div style={{ background: backgroundColor , height:'300px' }}>
+      <p> i am on  X:{width.widthX} and Y: {width.widthY}</p>
+    </div>
   )
 }
